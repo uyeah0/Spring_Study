a5fc4632ff8816a3ebd69f771af0dc2336c5f842
Yeyoung Yu <79914479+uyeah0@users.noreply.github.com>
2022-01-27 23:22:09 +0900

    220127 순수 JDBC

--- main/java/yeyeong/hellospring/SpringConfig.java
+++ main/java/yeyeong/hellospring/SpringConfig.java
@@ -1,18 +1,27 @@
 package yeyeong.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import yeyeong.hellospring.domain.Member;
+import yeyeong.hellospring.repository.JdbcMemberRepository;
 import yeyeong.hellospring.repository.MemberRepository;
 import yeyeong.hellospring.repository.MemoryMemberRepository;
 import yeyeong.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -20,6 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
 }
